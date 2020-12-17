be55d688fd76d88e6257604b93a89eb77136bd82
oksana <oksanastyles69@gmail.com>
2020-12-17 17:45:41 +0300

    3rd commit

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,21 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/right.png"></button>'
       });
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__list'),
+    menuItem = document.querySelectorAll('.menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu__list_active');
+        })
+    })
+})
